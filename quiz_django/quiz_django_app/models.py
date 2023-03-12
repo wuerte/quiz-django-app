e62d7530b484a591acab9d167edcf81c5c2e345a
Radosław Wierzgała <rad.wierzgala@gmail.com>
2023-03-12 23:07:28 +0100

    classes Game, Question created and registered in admin.py

--- quiz_django/quiz_django_app/models.py
+++ quiz_django/quiz_django_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    question_quantity = models.IntegerField()
+    actual_question = models.IntegerField(default=1)
+    total_score = models.IntegerField(default=0)
+
+
+class Question(models.Model):
+    correct_answer = models.IntegerField()
+    answer1 = models.CharField(max_length=255)
+    answer2 = models.CharField(max_length=255)
+    answer3 = models.CharField(max_length=255)
+    answer4 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "question nr: " + str(self.id)
