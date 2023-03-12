0601571d8ac4dacd2c2b48fe732da66d9b29b2c0
Radosław Wierzgała <rad.wierzgala@gmail.com>
2023-03-12 23:21:59 +0100

    Validation for correct_answer field setup, question_description field added to Question class

--- quiz_django/quiz_django_app/models.py
+++ quiz_django/quiz_django_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Game(models.Model):
     question_quantity = models.IntegerField()
@@ -7,11 +8,12 @@ class Game(models.Model):
 
 
 class Question(models.Model):
-    correct_answer = models.IntegerField()
-    answer1 = models.CharField(max_length=255)
-    answer2 = models.CharField(max_length=255)
-    answer3 = models.CharField(max_length=255)
-    answer4 = models.CharField(max_length=255)
+    correct_answer = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
+    question_description = models.CharField(max_length=255, null=True)
+    answer_1 = models.CharField(max_length=255)
+    answer_2 = models.CharField(max_length=255)
+    answer_3 = models.CharField(max_length=255)
+    answer_4 = models.CharField(max_length=255)
 
     def __str__(self):
         return "question nr: " + str(self.id)
