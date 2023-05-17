45f97115e93593242000360a45228137d459a447
Radosław Wierzgała <rad.wierzgala@gmail.com>
2023-05-17 22:24:17 +0200

    some changes

--- quiz_django/quiz_django/settings.py
+++ quiz_django/quiz_django/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'quiz_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "quiz",
+        "USER": "quiz",
+        "PASSWORD": "quiz",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
